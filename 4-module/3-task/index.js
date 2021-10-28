051c6756997e88ef9c39d0293aa64f5175731903
Victoria Chen <torichvic0@gmail.com>
2021-10-28 06:32:21 +0300

    4 module, task-3

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,20 @@
 function highlight(table) {
-  // ваш код...
+  let tbody = table.querySelector('tbody');
+
+  for (let row of tbody.rows) {
+    let ageCell = row.cells[1];
+    let genderCell = row.cells[2];
+    let statusCell = row.cells[3];
+
+    row.classList.add(statusCell.dataset.available === 'true' ? 'available' : 'unavailable');
+    row.classList.add(genderCell.innerHTML === 'm' ? 'male' : 'female');
+
+    if (statusCell.dataset.available === undefined) {
+      row.hidden = true;
+    }
+
+    if (parseInt(ageCell.innerHTML) < 18) {
+      row.style.textDecoration = 'line-through';
+    }
+  }
 }
