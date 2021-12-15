baa682776c1788cb07c027cc347ee9b4d1978762
Victoria Chen <torichvic0@gmail.com>
2021-12-15 19:02:59 +0300

    module-5, task-1

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,7 +1,16 @@
 function hideSelf() {
+<<<<<<< HEAD
   let button = document.querySelector('.hide-self-button');
 
   button.onclick = function() {
     this.hidden = true;
   };
+=======
+  // ваш код...
+  const btn1 = document.querySelector('.hide-self-button');
+
+  btn1.addEventListener('click', (event) => {
+    btn1.hidden = true;
+  });
+>>>>>>> fbe28ec (module-5, task-1)
 }
